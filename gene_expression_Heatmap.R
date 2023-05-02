d4b146d1015c3a9b3afa4c33d65f51e902939ac7
Hamizah <129884280+mizzx@users.noreply.github.com>
2023-05-02 09:08:37 +0800

    Update gene_expression_Heatmap.R

--- gene_expression_Heatmap.R
+++ gene_expression_Heatmap.R
@@ -4,63 +4,63 @@ library(limma)
 library(Biobase)
 library(gplots)
 
-#3. Import GSE file to extract phenoData
+#Import GSE file to extract phenoData
 bc <- getGEO("GSE137178")
 bc #this is preprocess file
 
 phenoData_bc <-phenoData(bc[[1]])
 phenoData_bc
 
-#4. Read .cel files into the environment.
+#Read .cel files into the environment.
 bc_cel <-ReadAffy()
 bc_cel #this is the raw file
 
-#5. Normalise
+#5Normalise
 eset_bc <-rma(bc_cel) #this is same like bc_cel
 eset_bc
 
-#6. Extract into expression dataset
+#Extract into expression dataset
 exprs_bc <-exprs(eset_bc)
 exprs_bc
 
-#7. Variables in the phenoData, use limma
+#Variables in the phenoData, use limma
 pheno_bc <-pData(phenoData_bc)
 
-#8. Extract out column of variable needed
+#Extract out column of variable needed
 # in pheno_bc last column, CXCR3i is disease, DMSO is normal
 #extract dubset from data frame, use square bracket
 pData_bc <- pheno_bc[,35] #here use all rows and column 35
 # here, it is string data type
 
-#9. Describe model to be fitted
+#Describe model to be fitted
 design_bc <- model.matrix(~pData_bc)
 design_bc
 
-#10. Fit each probeset to model
+#Fit each probeset to model
 fit_bc <- lmFit(eset_bc, design_bc)
 fit_bc 
 
-#11. Empirical Bayes adjustment
+#Empirical Bayes adjustment
 efit_bc <- eBayes(fit_bc)
 efit_bc #
 
-#12. Select the top 50 genes, limma analysis
+#Select the top 50 genes, limma analysis
 limma_bc <-topTable(efit_bc,number=50,coef=NULL,sort.by="P")
 #logFC differentiate genes based on given disease (like teruk ke tak)
 #downregulated, '-' sign in logFC.. upregulated, positive for logFC
 
 
-#13. Extract out the gene names of differential expressed genes 
+#Extract out the gene names of differential expressed genes 
 #from limma table formed in step 12.
 gname <- rownames(limma_bc)
 head(gname)
 
-#14. Attach the expression values of each genes 
+#Attach the expression values of each genes 
 #for each sample to form another dataset.
 bc_50 <- exprs_bc[c(gname),]
 head(bc_50)
 
-# 15. Conduct heatmap clustering, use gplots library
+# Conduct heatmap clustering, use gplots library
 # (note: refer to the heatmap function 
 # to play around with the scale, title, colour and etc.)
 heatmap(bc_50)
@@ -69,6 +69,6 @@ heatmap(bc_50)
 # darker colour is the genes are upregulated
 # lighter colour, the genes regulated
 
-# 16. questions
+#questions:
 #explain about the heatmap.. 
 #what colours represent up/down regulated
